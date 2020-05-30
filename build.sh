7b68ad91325be56fa1212993de58963203420b53
Anton Patsev <patsev.anton@gmail.com>
2020-05-30 13:00:17 +0600

    sudo yum-builddep -y teamcity-server-rpm.spec

--- build.sh
+++ build.sh
@@ -13,4 +13,5 @@ done
 mkdir -p ./{RPMS,SRPMS,BUILD,SOURCES,SPECS}
 cp teamcity-server.service ./SOURCES
 spectool -g -R teamcity-server-rpm.spec
+sudo yum-builddep -y teamcity-server-rpm.spec
 rpmbuild --quiet --define "_topdir `pwd`" -bb teamcity-server-rpm.spec
